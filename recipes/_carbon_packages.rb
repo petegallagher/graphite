8ea42c6a3f13c0817e1c1962deb2f0f2c2a8f5d4
Peter Gallagher <peter.gallagher@verint.com>
2015-02-22 10:46:53 +1000

    Only include  recipe if

--- recipes/_carbon_packages.rb
+++ recipes/_carbon_packages.rb
@@ -17,7 +17,7 @@
 # limitations under the License.
 #
 
-include_recipe "build-essential"
+include_recipe "build-essential" if node['graphite']['install_type'] == 'source'
 
 # sadly, have to pin Twisted to known good version
 # install before carbon so it's used
